b9e97065a15304e9e3e5aee1db69fc08645c325f
chrisvasqm <christianvasquezmelo@gmail.com>
2023-07-12 17:20:45 -0400

    Cleanup code

--- test/pokemon.test.js
+++ test/pokemon.test.js
@@ -3,14 +3,14 @@ const service = require('../src/services/pokemon-service');
 
 describe('Pokemon Tests', () => {
   it('Get all Pokemons', async () => {
-    await service
-      .getAll()
-      .then(response => expect(response.data.results).to.not.be.empty);
+    const response = await service.getAll();
+
+    expect(response.data.results).to.not.be.empty;
   });
 
   it("Read a Pokemon's name", async () => {
-    await service
-      .find(1)
-      .then(response => expect(response.data.name).to.be.equal('bulbasaur'));
+    const response = await service.find(1);
+
+    expect(response.data.name).to.be.equal('bulbasaur');
   });
 });
